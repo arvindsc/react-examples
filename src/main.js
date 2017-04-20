d9a2183a81e7081b7b798ec796c2efa4d86d4f24
arvindsc <arvindnitm@gmail.com>
2017-04-20 23:15:13 +0530

    add css loader to webpack

--- src/main.js
+++ src/main.js
@@ -2,24 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var MyTitleFactory = React.createFactory(MyTitle)
-
 var MyFirstComponent = React.createClass({
   render () {
     return (
-      React.DOM.div(null, [MyTitleFactory({
-        userName: 'Arvind',
-        color: 'peru'
-      }),
-        MyTitleFactory({
-          userName: 'Krati',
-          color: 'mediumaquamarine'
-        }),
-        MyTitleFactory({
-          userName: 'Askhat',
-          color: 'rebbcapurple'
-        })
-      ])
+      <div>
+        <MyTitle title= 'Title one'  color= 'peru'/>
+        <MyTitle title= 'Title two'  color= 'mediumaquamarine'/>
+        <MyTitle title= 'Title three'  color= 'rebbcapurple'/>
+      </div>
     )
   }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,23 @@ module.exports = () => {
   const config = {
     context: resolve('src'),
     entry: './main.js',
+
+    stats: {
+      colors: true,
+      reasons: true,
+      chunks: true
+    },
     output: {
       filename: 'bundle.js',
       path: resolve('dist'),
       publicPath: '/dist/'
+    },
+    module: {
+      rules: [{
+        exclude: '/node_modules/',
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }]
     }
   }
   return config
