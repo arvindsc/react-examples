3a7f098ec787b4644f06796761e2e11dafa6ffe2
arvindsc <arvindnitm@gmail.com>
2017-04-22 23:31:56 +0530

    5

--- src/Search.js
+++ src/Search.js
@@ -1,17 +1,37 @@
 import React from 'react'
 import preload from '../dist/data.json'
 import ShowCard from './ShowCard'
+import {string} from 'prop-types'
 
 class Search extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchTerm: 'default searvh string'
+    }
+  }
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  }
+
   render () {
     return (
       <div className='search'>
-        {preload.shows.map(function (show) {
-          return <ShowCard key={show.imdbID} {...show} />
-        }
-        )}
+        <header>
+          <h1>nplayer</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
+        </header>
+        <div>
+          {preload.shows.map(function (show) {
+            return <ShowCard key={show.imdbID} {...show} />
+          })}
+        </div>
+
       </div>
     )
   }
+}
+Search.propTypes = {
+  searchTerm: string
 }
 export default Search
