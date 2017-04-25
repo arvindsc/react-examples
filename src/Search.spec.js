ec2e533f35065d6edcd6786b4be98bf09a665468
arvindsc <arvindnitm@gmail.com>
2017-04-25 11:37:45 +0530

    add test cases for show search functionality

--- src/Search.spec.js
+++ src/Search.spec.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import Search from './Search'
-import renderer from 'react-test-renderer'
+import ShowCard from './ShowCard'
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
+import preload from '../dist/data.json'
 
 test('Sear snapshot test', () => {
-  const component = renderer.create(<Search />)
-  const tree = component.toJSON()
+  const component = shallow(<Search />)
+  const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
+})
+
+test('Search should render ShowCard for each show', () => {
+  const component = shallow(<Search />)
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+})
+
+test('Search should render a correct amount of shows based on search', () => {
+  const searchWord = 'house'
+  const component = shallow(<Search />)
+  component.find('input').simulate('change', { target: { value: searchWord } })
+
+  const showCount = preload.shows.filter((show) => {
+    return `${show.title} ${show.description}`.toUpperCase()
+      .indexOf(searchWord.toUpperCase()) >= 0
+  }).length
+  expect(component.find(ShowCard).length).toEqual(showCount)
 })
