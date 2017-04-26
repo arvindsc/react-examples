ffbf8aac2b7dc24f83252d2ba062e1336b4439b3
arvindsc <arvindnitm@gmail.com>
2017-04-26 23:59:01 +0530

    add header component

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Link} from 'react-router'
+
+class Header extends React.Component{
+    render(){
+        return (  <h1>nvideo</h1>)
+    }
+}
+export default Header
